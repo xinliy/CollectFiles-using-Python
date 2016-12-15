636e52f733cb16e70af8db3190da0d611f73164b
xinliy <584827526@qq.com>
2016-12-15 14:55:12 +0800

    Now can copy it to one folder

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ def findFile(yourpath):   #input a origin path
 	
 	for i in filelist:                          #Read everything in this path
 		d=i.find('txt')                         #In this case I want to find '.txt'files, you can change it to 'jpg',etc
-		if d != -1 :               
+		if d != -1 : 
+			truepath = yourpath+"\\"+i
+			shutil.copy(truepath,'D:\\test1')
 			print(i)
 	                                            #print needed files.
 		path2 = yourpath +"\\" +i
+	
 		                                        #go into sub path
 		                                        #print ('path2 is:' + path2)
 		if os.path.isdir(path2):                #If the sub path is a dir, call the function again.
