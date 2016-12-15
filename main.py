085b42d76c723009be22e57cdebbcc02602ca05e
xinliy <584827526@qq.com>
2016-12-15 14:32:28 +0800

    One

---
+++ main.py
@@ -0,0 +1,28 @@
+import os,shutil
+
+#The code is used to find any files like '.txt','.jpg',and print these files			
+
+def findFile(yourpath):   #input a origin path
+	
+	filelist = os.listdir(yourpath)
+	
+	for i in filelist:                          #Read everything in this path
+		d=i.find('txt')                         #In this case I want to find '.txt'files, you can change it to 'jpg',etc
+		if d != -1 :               
+			print(i)
+	                                            #print needed files.
+		path2 = yourpath +"\\" +i
+		                                        #go into sub path
+		                                        #print ('path2 is:' + path2)
+		if os.path.isdir(path2):                #If the sub path is a dir, call the function again.
+			                                    #print("I find a dir path!It's:")
+			                                    #print(path2)
+			findFile(path2)
+		
+			
+path ='D:\\'
+
+
+findFile(path)
+
+
