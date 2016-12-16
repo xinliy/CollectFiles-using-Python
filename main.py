8923cf5c6c5cac98c1636a59869376c8fa815eba
xinliy <584827526@qq.com>
2016-12-16 17:51:53 +0800

    Find Videos

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import os,shutil
 
-#The code is used to find any files like '.txt','.jpg',and print these files	
+#The code is used to find any files like '.txt','.jpg',and print these files
 
 
 
 def findFile(yourpath):   #input a origin path
 	try:
 		filelist = os.listdir(yourpath)
-		
+
 	except:
-		print('one')
+		print('Cannot open the dir!')
 		filelist = []     #without this the function will fail when error happened.
 		pass
 
-	for i in filelist: 
-		print("2222")
+	for i in filelist:
+		#print("2222")
 										#Read everything in this path
-		d=i.find('txt')
-		print(d)
-        		
-		
-		if d != -1:
+		d=i.find('.avi')
+		d1=i.find('.mp4')
+		d3=i.find('.mkv')
+		d2=i.find('.rmvb')
+
+
+
+		#print(d)
+
+
+		if d != -1 or d1 != -1 or d2 != -1 or d3 != -1:
+			print("the target file is "+i)
 			truepath = yourpath+"\\"+i
 			try:
-				shutil.move(truepath,'D:\\test\\test1')      #the destination path
-			
+				shutil.move(truepath,'G:\\Videos')      #the destination path
+
 			except:
 				print('error!')
 				pass
@@ -37,12 +44,12 @@ def findFile(yourpath):   #input a origin path
 			                                    #print("I find a dir path!It's:")
 			                                    #print(path2)
 			findFile(path2)
-		
-			
-			
 
-			
-path ='E:\\迅雷下载'     #the target main path
+
+
+
+
+path ='G:\\'     #the target main path
 
 
 
